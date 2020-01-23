fb6001df64eda2ea1a6f84c5a6f1adf6c99e687b
Felipe Manso <felipe.manso@saganet.com.br>
2020-01-23 16:20:50 -0300

    Testando primeira classe de dominio

--- src/main/java/com/manso/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/manso/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,12 @@
 package com.manso.cursomc.resources;
 
+import com.manso.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -10,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest Funfou";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "informatica");
+        Categoria cat2 = new Categoria(2, "escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
