343fc0fc1924c894e2f9e6beb567230b61e19bfd
Felipe Manso <felipe.manso@saganet.com.br>
2020-01-24 14:48:26 -0300

    Produtos e associação muitos para muitos

---
+++ src/main/java/com/manso/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.manso.cursomc.repositories;
+
+import com.manso.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
