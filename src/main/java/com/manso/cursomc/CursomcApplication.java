a191e64e4b40e822a86cc62aa99d8098af95b30b
Felipe Manso <felipe.manso@saganet.com.br>
2020-01-27 09:15:18 -0300

    Estado e Cidade

--- src/main/java/com/manso/cursomc/CursomcApplication.java
+++ src/main/java/com/manso/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.manso.cursomc;
 
 import com.manso.cursomc.domain.Categoria;
+import com.manso.cursomc.domain.Cidade;
+import com.manso.cursomc.domain.Estado;
 import com.manso.cursomc.domain.Produto;
 import com.manso.cursomc.repositories.CategoriaRepository;
+import com.manso.cursomc.repositories.CidadeRepository;
+import com.manso.cursomc.repositories.EstadoRepository;
 import com.manso.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,8 +19,13 @@ import java.util.Arrays;
 public class CursomcApplication implements CommandLineRunner {
 
     @Autowired
-    private CategoriaRepository categoriaRepository;@Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
     private ProdutoRepository produtoRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -41,5 +50,20 @@ public class CursomcApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "Sao Paulo");
+
+        Cidade c1 = new Cidade("Uberlandia", est1);
+        Cidade c2 = new Cidade("Sao Paulo", est2);
+        Cidade c3 = new Cidade("Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1,est2));
+        cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
+
     }
 }
