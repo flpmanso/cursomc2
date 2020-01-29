d53c8200bea65a36a51c5d425157f8317a961bdc
Felipe Manso <felipe.manso@saganet.com.br>
2020-01-29 09:30:49 -0300

    Listando categorias com DTO

--- src/main/java/com/manso/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/manso/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.manso.cursomc.resources;
 
 import com.manso.cursomc.domain.Categoria;
+import com.manso.cursomc.dto.CategoriaDTO;
 import com.manso.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -42,5 +45,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> delete(@PathVariable Integer id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
--- src/main/java/com/manso/cursomc/services/CategoriaService.java
+++ src/main/java/com/manso/cursomc/services/CategoriaService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,6 +23,10 @@ public class CategoriaService {
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()
         ));
+    }
+
+    public List<Categoria> findAll() {
+        return repo.findAll();
     }
 
     public Categoria insert(Categoria obj) {
@@ -38,8 +43,7 @@ public class CategoriaService {
         find(id);
         try {
             repo.deleteById(id);
-        }
-        catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("não é possivel excluir uma categoria que possui produtos.");
         }
     }
