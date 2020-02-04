78008cce2d16088dde3113a701df48fefe4a53e4
Felipe Manso <felipe.manso@saganet.com.br>
2020-02-04 10:44:26 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/manso/cursomc/services/CategoriaService.java
+++ src/main/java/com/manso/cursomc/services/CategoriaService.java
@@ -5,8 +5,12 @@ import com.manso.cursomc.exception.DataIntegrityException;
 import com.manso.cursomc.exception.ObjectNotFoundException;
 import com.manso.cursomc.repositories.CategoriaRepository;
 import com.manso.cursomc.resources.CategoriaResource;
+import com.sun.javafx.scene.traversal.Direction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,6 +31,11 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return repo.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 
     public Categoria insert(Categoria obj) {
